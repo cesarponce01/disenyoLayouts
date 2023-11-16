9a55c35078d4af6cc85712dd90d409b0b901c250
cesarponce01 <cesarinnj01@gmail.com>
2023-11-16 10:09:36 +0100

    Layouts

---
+++ app/src/main/java/com/example/disenyo14_11_2023/Actividad6.kt
@@ -0,0 +1,11 @@
+package com.example.disenyo14_11_2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Actividad6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad6)
+    }
+}
---
+++ app/src/main/java/com/example/disenyo14_11_2023/otrosLayouts3.kt
@@ -0,0 +1,11 @@
+package com.example.disenyo14_11_2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class otrosLayouts3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otros_layouts3)
+    }
+}
